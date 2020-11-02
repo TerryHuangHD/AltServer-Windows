0bdc116efb33acd39f7b59211e5aea6ea8f1f87f
Terry Huang <kmshiori@gmail.com>
2020-11-02 16:10:08 +0800

    Fix Merge

--- AltServer/Semaphore.h
+++ AltServer/Semaphore.h
@@ -4,7 +4,6 @@
 //
 //  Created by Riley Testut on 10/7/20.
 //  Copied from https://stackoverflow.com/a/19659736
-<<<<<<< HEAD
 //
 
 #pragma once
@@ -38,39 +37,4 @@ private:
 	std::mutex mtx;
 	std::condition_variable cv;
 	int count;
-=======
-//
-
-#pragma once
-
-#include <mutex>
-#include <condition_variable>
-
-class Semaphore {
-public:
-	Semaphore(int count_ = 0)
-		: count(count_) {}
-
-	inline void notify()
-	{
-		std::unique_lock<std::mutex> lock(mtx);
-		count++;
-		cv.notify_one();
-	}
-
-	inline void wait()
-	{
-		std::unique_lock<std::mutex> lock(mtx);
-
-		while (count <= 0) {
-			cv.wait(lock);
-		}
-		count--;
-	}
-
-private:
-	std::mutex mtx;
-	std::condition_variable cv;
-	int count;
->>>>>>> upstream/master
 };
